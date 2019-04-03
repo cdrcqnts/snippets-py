3c7e30ed28a3757c67d98f37a681c996b851a445
cdrcqnts <cdrcqnts@gmail.com>
2019-04-03 14:30:10 +0200

    code optimization

--- better_code.py
+++ better_code.py
@@ -21,3 +21,50 @@ num2 = 100_000_000
 total = num1 + num2
 
 print(f"{total:,}")  # print total as an f-string, use colons to format output
+
+
+""" OPENING AND CLOSING RESOURCES (FILES, DB, ...) WITH CONTEXT MANAGER
+"""
+
+with open("./test.txt", "r") as f:
+    file_content = f.read()
+
+# no need to .close() the file
+
+words = file_content.split(" ")
+word_count = len(words)
+print(word_count)
+
+""" ENUMERATE WITH CUSTOM INDEX STARTING POINT
+"""
+names = ["Anna", "Bob", "Charles"]
+names_indexed = [(i, n) for i, n in enumerate(names, start=1)]
+print(names_indexed)
+
+""" ITERATE OVER MULTIPLE LISTS PAIRWISE
+"""
+animals = ["Dog", "Cat", "Bird", "Worm"]
+legs = [4, 4, 2, 0]
+fur = [1, 1, 0, 0]
+animals_zipped = [(a, l, f) for a, l, f in zip(animals, legs, fur)]
+print(animals_zipped)
+
+""" UNPACKING
+"""
+
+# normal
+items = (1, 2, 3, 4, 5, 6, 7)
+print(items)
+
+# unpacking
+a, _, *c, d = items
+print(a)
+print(c)
+print(d)
+
+""" LIST VALID ATTRIBUTES / METHODS
+"""
+
+from datetime import datetime
+
+print(dir(datetime))
